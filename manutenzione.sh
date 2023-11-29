2d8a3b9791e6216a35758006704f44e63503bb84
GjMan78 <116692121+GjMan78@users.noreply.github.com>
2023-11-29 14:45:56 +0100

    Funzioni aggiuntive
    Salvataggio e ripristino pacchetti installati con dpkg.

--- manutenzione.sh
+++ manutenzione.sh
@@ -40,13 +40,14 @@ fi
 
 Disclaimer () {
 
-dialog --backtitle "Dichiarazione di NON responsabilità" \
---title "LEGGI BENE!" \
---msgbox "L'autore declina ogni responsabilità per eventuali danni diretti o indiretti e/o malesseri fisici o psicologici causati dallo script. \n\n
-Proseguendo accetti di utilizzare il software fornito a tuo rischio e pericolo e dichiari di essere l'unico responsabile dei possibili danni derivanti dall'uso dello stesso.\n\n 
-Non si accettano lamentele se il cane miagola o il gatto abbaia durante l'esecuzione del programma.\n\n
-Può causare sonnolenza. Leggere attentamente il foglietto illustrativo.\n\n
-Have Fun! \n" 0 0 
+dialog \
+  --backtitle "Dichiarazione di NON responsabilità" \
+  --title "LEGGI BENE!" \
+  --msgbox "L'autore declina ogni responsabilità per eventuali danni diretti o indiretti e/o malesseri fisici \
+	o psicologici causati dallo script.\n\nProseguendo accetti di utilizzare il software fornito a tuo rischio e \
+	pericolo e dichiari di essere l'unico responsabile dei possibili danni derivanti dall'uso dello stesso.\n
+	\nNon si accettano lamentele se il cane miagola o il gatto abbaia durante l'esecuzione del programma.\n
+	\nPuò causare sonnolenza. Leggere attentamente il foglietto illustrativo.\n\nHave Fun! \n" 0 0 
 
 }
 
@@ -74,13 +75,15 @@ Requisiti (){
 
 Aggiornamento (){
 
-dialog --stdout --backtitle "Script Manutenzione Ubuntu" --title "Conferma" --yesno "Confermi l'esecuzione di questi comandi?\n\n
+dialog \
+  --stdout \
+  --backtitle "Script Manutenzione Ubuntu" \
+  --title "Conferma" \
+  --yesno "Confermi l'esecuzione di questi comandi?\n\n
 apt update \n
 apt -y dist-upgrade" 0 0
 
-  resp=$?
-
-  if [ $resp -eq 1 ]; then
+  if [ $? -eq 1 ]; then
         return
   fi
 
@@ -99,9 +102,15 @@ apt -y dist-upgrade" 0 0
   echo -e "\n\n ****** FINE LOG AGGIORNAMENTO SISTEMA ****** ${data} ******\n\n" | sudo -u ${utente} tee -a ${logfile}
 
   if [ $cmd1 -eq 0 ] && [ $cmd2 -eq 0 ]; then
-    dialog --backtitle "Script Manutenzione Ubuntu" --title "Successo" --msgbox "Aggiornamento del sistema eseguito con successo!" 0 0
+    dialog \
+	--backtitle "Script Manutenzione Ubuntu" \
+	--title "Successo" \
+	--msgbox "Aggiornamento del sistema eseguito con successo!" 0 0
   else
-    dialog --backtitle "Script Manutenzione Ubuntu" --title "Errore" --msgbox "Ops...Qualcosa è andato storto!" 0 0
+    dialog \
+	--backtitle "Script Manutenzione Ubuntu" \
+	--title "Errore" \
+	--msgbox "Ops...Qualcosa è andato storto!" 0 0
   fi
 }
 
@@ -109,13 +118,13 @@ apt -y dist-upgrade" 0 0
 # Da migliorare!
 
 PuliziaTemporanei () {
-dialog --stdout --backtitle "Script Manutenzione Ubuntu" --title "Conferma" --yesno "Confermi l'esecuzione di questi comandi? \n\n
-rm -rf /home/$USER/.cache/* \n
-apt purge '?config-files'" 0 0
+dialog \
+  --stdout \
+  --backtitle "Script Manutenzione Ubuntu" \
+  --title "Conferma" \
+  --yesno "Confermi l'esecuzione di questi comandi? \n\n rm -rf /home/$USER/.cache/* \n apt purge '?config-files'" 0 0
 
-  resp=$?
-
-  if [ $resp -eq 1 ]; then
+  if [ $? -eq 1 ]; then
         return
   fi
 
@@ -134,22 +143,27 @@ apt purge '?config-files'" 0 0
   echo -e "\n\n ****** FINE LOG PULIZIA FILE TEMPORANEI ****** ${data} ******\n\n" | sudo -u ${utente} tee -a ${logfile}
 
   if [ $cmd1 -eq 0 ] && [ $cmd2 -eq 0 ]; then
-	dialog --backtitle "Script Manutenzione Ubuntu" --title "Successo" --msgbox "Eliminazione file temporanei completata" 0 0
+	dialog \
+	  --backtitle "Script Manutenzione Ubuntu" \
+	  --title "Successo" \
+	  --msgbox "Eliminazione file temporanei completata" 0 0
   else
-	dialog --backtitle "Script Manutenzione Ubuntu" --title "Errore" --msgbox "Ops...Qualcosa è andato storto!" 0 0
+	dialog \
+	  --backtitle "Script Manutenzione Ubuntu" \
+	  --title "Errore" \
+	  --msgbox "Ops...Qualcosa è andato storto!" 0 0
   fi
 }
 
 RiparazionePacchetti (){
 
-dialog --stdout --backtitle "Script Manutenzione Ubuntu" --title "Conferma" --yesno "Confermi l'esecuzione di questi comandi? \n\n
-apt install -f \n
-dpkg --configure -a \n
-" 0 0
+dialog \
+  --stdout \
+  --backtitle "Script Manutenzione Ubuntu" \
+  --title "Conferma" \
+  --yesno "Confermi l'esecuzione di questi comandi? \n\n apt install -f \n dpkg --configure -a \n" 0 0
 
-  resp=$?
-
-  if [ $resp -eq 1 ]; then
+  if [ $? -eq 1 ]; then
         return
   fi
 
@@ -168,9 +182,14 @@ dpkg --configure -a \n
   echo -e "\n\n ****** FINE LOG RIPARAZIONE PACCHETTI ****** ${data} ******\n\n" | sudo -u ${utente} tee -a ${logfile}
 
   if [ $cmd1 -eq 0 ] && [ $cmd2 -eq 0 ]; then
-        dialog --backtitle "Script Manutenzione Ubuntu" --title "Successo" --msgbox "Riparazione pacchetti  completata" 0 0
+        dialog \
+	  --backtitle "Script Manutenzione Ubuntu" \
+	  --title "Successo" \
+	  --msgbox "Riparazione pacchetti  completata" 0 0
   else
-        dialog --backtitle "Script Manutenzione Ubuntu" --title "Errore" --msgbox "Ops...Qualcosa è andato storto!" 0 0
+        dialog \
+	  --backtitle "Script Manutenzione Ubuntu" \
+	  --title "Errore" --msgbox "Ops...Qualcosa è andato storto!" 0 0
   fi
 }
 
@@ -178,18 +197,16 @@ dpkg --configure -a \n
 
 PuliziaPacchetti () {
 
-dialog --stdout --backtitle "Script Manutenzione Ubuntu" --title "Conferma" --yesno "Confermi l'esecuzione di questi comandi?\n
-apt clean \n
-apt autoclean \n
-apt autoremove \n
-apt autopurge \n
+dialog \
+  --stdout \
+  --backtitle "Script Manutenzione Ubuntu" \ 
+  --title "Conferma" \
+  --yesno "Confermi l'esecuzione di questi comandi?\n apt clean \n apt autoclean \n apt autoremove \n apt autopurge \n
 deborphan | xargs apt -y purge \n
 apt purge '?config-files'\n
 journalctl --rotate --vacuum-size=500M" 0 0
 
-  resp=$?
-
-  if [ $resp -eq 1 ]; then
+  if [ $? -eq 1 ]; then
         return
   fi
 
@@ -223,9 +240,15 @@ data=$(date)
 echo -e "\n\n ****** FINE LOG PULIZIA PACCHETTI OBSOLETI ****** ${data} ******\n\n" | sudo -u ${utente} tee -a ${logfile}
 
 if [ $cmd1 -eq 0 ] && [ $cmd2 -eq 0 ] && [ $cmd3 -eq 0 ] && [ $cmd4 -eq 0 ] && [ $cmd5 -eq 0 ] && [ $cmd6 -eq 0 ] && [ $cmd7 -eq 0 ]; then
-    dialog --backtitle "Script Manutenzione Ubuntu" --title "Successo" --msgbox "Pulizia dei pacchetti eseguita con successo!" 0 0
+    dialog \
+	--backtitle "Script Manutenzione Ubuntu" \
+	--title "Successo" \
+	--msgbox "Pulizia dei pacchetti eseguita con successo!" 0 0
   else
-    dialog --backtitle "Script Manutenzione Ubuntu" --title "Errore" --msgbox "Ops...Qualcosa è andato storto!" 0 0
+    dialog \
+	--backtitle "Script Manutenzione Ubuntu" \
+	--title "Errore" \
+	--msgbox "Ops...Qualcosa è andato storto!" 0 0
   fi
 
 }
@@ -235,27 +258,90 @@ ApriLog () {
 if [  -f ${logfile} ]; then
 	sudo -u ${utente} xdg-open ${logfile} > /dev/null 2>&1
 else
-	dialog --title "Errore" --backtitle "Script Manutenzione Ubuntu" --msgbox "Il file non esiste. Devi prima eseguire una delle operazioni di manutenzione" 0 0
+	dialog \
+	  --title "Errore" \
+	  --backtitle "Script Manutenzione Ubuntu" \
+	  --msgbox "Il file non esiste. Devi prima eseguire una delle operazioni di manutenzione" 0 0
 fi
 
 return
 
 }
 
+FunzioniUtili () {
+
+cmdF=(dialog \
+	--ok-label "Esegui" \
+	--cancel-label "Indietro" \
+	--backtitle "Script Manutenzione Ubuntu" \
+	--menu "Funzioni (forse) Utili" 0 0 0)
+optionsF=(1 "Salva elenco pacchetti installati"
+          2 "Installa pacchetti da elenco salvato")
+
+choices=$("${cmdF[@]}" "${optionsF[@]}" 2>&1 >/dev/tty)
+
+if [ $? -eq 1 ]; then
+      clear; return
+fi
+
+for choice in ${choices}; do 
+        case ${choice} in
+          1) 
+	    sudo -u ${SUDO_USER} dpkg --get-selections > /home/${SUDO_USER}/installed-software.log 
+            dialog \
+		--ok-label "Chiudi" \
+		--extra-button \
+		--extra-label "Apri File" \
+		--title "Esportazione completata" \
+		--backtitle "Script Manutenzione Ubuntu" \
+		--msgbox "L'elenco dei pacchetti installati è stato salvato nella tua /home" 0 0
+
+	    if [ $? -eq 0 ]; then
+		clear; return
+	    else
+		sudo -u ${SUDO_USER} xdg-open /home/${SUDO_USER}/installed-software.log > /dev/null 2>&1
+	    fi
+	    ;;
+          2)
+            if  [ -e /home/${SUDO_USER}/installed-software.log ]; then
+		dialog \
+                    --title "Attenzione!" \
+                    --backtitle "Script Manutenzione Ubuntu" \
+                    --yesno "Funzione non testata proseguire ugualmente?" 0 0
+		if [ ! $? -eq 0 ]; then
+			return
+		fi
+		dpkg --set-selections < /home/${SUDO_USER}/installed-software.log && apt-get dselect-upgrade
+	    else
+	    	dialog \
+ 	      	    --title "Errore!" \
+	      	    --backtitle "Script Manutenzione Ubuntu" \
+	            --msgbox "Manca il file installed-software.log" 0 0
+	    fi 
+	    ;;
+        esac 
+  done
+
+}
 # Menu principale dello script
 
 Menu () {
-  resp=''
-  choice=''
-  choices="$(dialog --ok-label "Esegui" --cancel-label "Esci" --backtitle "Script Manutenzione Ubuntu" --stdout --checklist "Manutenzione del sistema:" 0 0 0 \
-    1 "Aggiornamento del sistema" off \
-    2 "Eliminazione file temporanei" off \
-    3 "Eliminazione pacchetti orfani e vecchi log" off \
-    4 "Riparazione pacchetti bloccati o danneggiati" off \
-    5 "Apri log delle operazioni eseguite" off )"
 
-  resp=$?
-  if [ $resp -eq 1 ]; then
+  cmd=(dialog \
+	--ok-label "Esegui" \
+	--cancel-label "Esci" \
+	--backtitle "Script Manutenzione Ubuntu" \
+	--menu "Manutenzione del sistema" 0 0 0)
+  options=(1 "Aggiornamento del sistema"
+           2 "Eliminazione file temporanei"
+           3 "Eliminazione pacchetti orfani e vecchi log"
+           4 "Riparazione pacchetti bloccati o danneggiati"
+           5 "Apri log delle operazioni effettuate"
+           6 "Funzioni (forse) utili")
+
+  choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+
+  if [ $? -eq 1 ]; then
         clear; exit
   fi
 
@@ -266,7 +352,8 @@ Menu () {
           2) PuliziaTemporanei ;;
           3) PuliziaPacchetti ;;
           4) RiparazionePacchetti ;;
-	  5) ApriLog;;
+	  5) ApriLog ;;
+	  6) FunzioniUtili :;
         esac 
   done
 }
