ffa698ed2173397161eec4330d8e5c5d54de706b
GjMan78 <116692121+GjMan78@users.noreply.github.com>
2023-11-29 07:29:54 +0100

    Revert "Implementato il log"
    This reverts commit 65feb88e68620c9fe2194df60a72811e31c9725c.

--- manutenzione.sh
+++ manutenzione.sh
@@ -2,37 +2,15 @@
 
 # Autore: GjMan78 per forum.ubuntu.it.org
 
-# Ultimo aggiornamento: 29-11-2023
+# Ultimo aggiornamento: 28-11-2023
 
 # Script PARECCHIO work in progress. 
 
 # DA IMPLEMENTARE: 
-# 
+# - gestione dei log di ogni comando, utli per  debug in caso di errori.
 # - eventuale progress bar (dialog --gauge ??)
 
-# - funzioni utili per il forum (diagnosi aggiornamenti - diagnosi rete)
-# ls -l /etc/apt/sources.list.d | awk '{ print $9 }'
-
 # DA MIGLIORARE: pulizia file temporanei
-
-
-# Log eventi
-InizializzaLog () {
-
-utente=${SUDO_USER}
-percorso="/home/${utente}/.config/puliziaubuntu/"
-nomefile=$(date +%Y-%m-%d)
-logfile="${percorso}${nomefile}.log"
-
-# crea la cartella delle configurazioni se non esiste
-if [ ! -d ${percorso} ]; then
-  sudo -u ${utente} mkdir ${percorso}
-else
-# elimina log più vecchi di 7 giorni (se esistono)
-  find ${percorso} -type f -mtime +7 -name '*.log' -print0 | xargs -r0 rm -- > /dev/null
-fi
-
-}
 
 
 # Consenso informato
@@ -45,7 +23,7 @@ dialog --backtitle "Dichiarazione di NON responsabilità" \
 Proseguendo accetti di utilizzare il software fornito a tuo rischio e pericolo e dichiari di essere l'unico responsabile dei possibili danni derivanti dall'uso dello stesso.\n\n 
 Non si accettano lamentele se il cane miagola o il gatto abbaia durante l'esecuzione del programma.\n\n
 Può causare sonnolenza. Leggere attentamente il foglietto illustrativo.\n\n
-Have Fun! \n" 0 0 
+Have Fun! \n" 0 0
 
 }
 
@@ -58,13 +36,13 @@ Requisiti (){
   pkg=dialog
   status="$(dpkg-query -W --showformat='${db:Status-Status}' "$pkg" 2>&1)"
   if [ ! $? = 0 ] || [ ! "$status" = installed ]; then
-     apt -y install $pkg 
+     apt -y install $pkg
   fi
 
   pkg=deborphan
   status="$(dpkg-query -W --showformat='${db:Status-Status}' "$pkg" 2>&1)"
   if [ ! $? = 0 ] || [ ! "$status" = installed ]; then
-     apt -y install $pkg 
+     apt -y install $pkg
   fi
 
 }
@@ -73,7 +51,7 @@ Requisiti (){
 
 Aggiornamento (){
 
-dialog --stdout --backtitle "Script Manutenzione Ubuntu" --title "Conferma" --yesno "Confermi l'esecuzione di questi comandi?\n\n
+dialog --stdout --yesno "Confermi l'esecuzione di questi comandi?\n\n
 apt update \n
 apt -y dist-upgrade" 0 0
 
@@ -85,22 +63,16 @@ apt -y dist-upgrade" 0 0
 
   clear
 
-  data=$(date)
-  echo -e "\n\n ****** INIZIO LOG AGGIORNAMENTO SISTEMA ****** ${data} ******\n\n" | sudo -u ${utente} tee -a ${logfile}
-
-  apt update | sudo -u ${utente} tee -a ${logfile}
+  apt update
   cmd1=$?
 
-  apt -y dist-upgrade | sudo -u ${utente} tee -a ${logfile}
+  apt -y dist-upgrade
   cmd2=$?
 
-  data=$(date)
-  echo -e "\n\n ****** FINE LOG AGGIORNAMENTO SISTEMA ****** ${data} ******\n\n" | sudo -u ${utente} tee -a ${logfile}
-
   if [ $cmd1 -eq 0 ] && [ $cmd2 -eq 0 ]; then
-    dialog --backtitle "Script Manutenzione Ubuntu" --title "Successo" --msgbox "Aggiornamento del sistema eseguito con successo!" 0 0
+    dialog --msgbox "Aggiornamento del sistema eseguito con successo!" 0 0
   else
-    dialog --backtitle "Script Manutenzione Ubuntu" --title "Errore" --msgbox "Ops...Qualcosa è andato storto!" 0 0
+    dialog --msgbox "Ops...Qualcosa è andato storto!" 0 0
   fi
 }
 
@@ -108,7 +80,7 @@ apt -y dist-upgrade" 0 0
 # Da migliorare!
 
 PuliziaTemporanei () {
-dialog --stdout --backtitle "Script Manutenzione Ubuntu" --title "Conferma" --yesno "Confermi l'esecuzione di questi comandi? \n\n
+dialog --stdout --yesno "Confermi l'esecuzione di questi comandi? \n\n
 rm -rf /home/$USER/.cache/* \n
 apt purge '?config-files'" 0 0
 
@@ -120,28 +92,22 @@ apt purge '?config-files'" 0 0
 
   clear
 
-  data=$(date)
-  echo -e "\n\n ****** INIZIO LOG PULIZIA FILE TEMPORANEI ****** ${data} ******\n\n" | sudo -u ${utente} tee -a ${logfile}
-
-  rm -rf /home/$USER/.cache/* | sudo -u ${utente} tee -a ${logfile}
+  rm -rf /home/$USER/.cache/*
   cmd1=$?
 
-  apt purge '?config-files' | sudo -u ${utente} tee -a ${logfile}
+  apt purge '?config-files'
   cmd2=$?
 
-  data=$(date)
-  echo -e "\n\n ****** FINE LOG PULIZIA FILE TEMPORANEI ****** ${data} ******\n\n" | sudo -u ${utente} tee -a ${logfile}
-
   if [ $cmd1 -eq 0 ] && [ $cmd2 -eq 0 ]; then
-	dialog --backtitle "Script Manutenzione Ubuntu" --title "Successo" --msgbox "Eliminazione file temporanei completata" 0 0
+	dialog --msgbox "Eliminazione file temporanei completata" 0 0
   else
-	dialog --backtitle "Script Manutenzione Ubuntu" --title "Errore" --msgbox "Ops...Qualcosa è andato storto!" 0 0
+	dialog --msgbox "Ops...Qualcosa è andato storto!" 0 0
   fi
 }
 
 RiparazionePacchetti (){
 
-dialog --stdout --backtitle "Script Manutenzione Ubuntu" --title "Conferma" --yesno "Confermi l'esecuzione di questi comandi? \n\n
+dialog --stdout --yesno "Confermi l'esecuzione di questi comandi? \n\n
 apt install -f \n
 dpkg --configure -a \n
 " 0 0
@@ -154,22 +120,16 @@ dpkg --configure -a \n
 
   clear
 
-  data=$(date)
-  echo -e "\n\n ****** INIZIO LOG RIPARAZIONE PACCHETTI ****** ${data} ******\n\n" | sudo -u ${utente} tee -a ${logfile}
-
-  apt install -f | sudo -u ${utente} tee -a ${logfile}
+  apt install -f
   cmd1=$?
 
-  dpkg --configure -a | sudo -u ${utente} tee -a ${logfile}
+  dpkg --configure -a
   cmd2=$?
 
-  data=$(date)
-  echo -e "\n\n ****** FINE LOG RIPARAZIONE PACCHETTI ****** ${data} ******\n\n" | sudo -u ${utente} tee -a ${logfile}
-
   if [ $cmd1 -eq 0 ] && [ $cmd2 -eq 0 ]; then
-        dialog --backtitle "Script Manutenzione Ubuntu" --title "Successo" --msgbox "Riparazione pacchetti  completata" 0 0
+        dialog --msgbox "Riparazione pacchetti  completata" 0 0
   else
-        dialog --backtitle "Script Manutenzione Ubuntu" --title "Errore" --msgbox "Ops...Qualcosa è andato storto!" 0 0
+        dialog --msgbox "Ops...Qualcosa è andato storto!" 0 0
   fi
 }
 
@@ -177,7 +137,7 @@ dpkg --configure -a \n
 
 PuliziaPacchetti () {
 
-dialog --stdout --backtitle "Script Manutenzione Ubuntu" --title "Conferma" --yesno "Confermi l'esecuzione di questi comandi?\n
+dialog --stdout --yesno "Confermi l'esecuzione di questi comandi?\n
 apt clean \n
 apt autoclean \n
 apt autoremove \n
@@ -193,9 +153,6 @@ journalctl --rotate --vacuum-size=500M" 0 0
   fi
 
 clear
-
-data=$(date)
-echo -e "\n\n ****** INIZIO LOG PULIZIA PACCHETTI OBSOLETI ****** ${data} ******\n\n" | sudo -u ${utente} tee -a ${logfile}
 
 apt -y autoremove
 cmd1=$?
@@ -218,26 +175,11 @@ cmd6=$?
 journalctl --rotate --vacuum-size=500M
 cmd7=$?
 
-data=$(date)
-echo -e "\n\n ****** FINE LOG PULIZIA PACCHETTI OBSOLETI ****** ${data} ******\n\n" | sudo -u ${utente} tee -a ${logfile}
-
 if [ $cmd1 -eq 0 ] && [ $cmd2 -eq 0 ] && [ $cmd3 -eq 0 ] && [ $cmd4 -eq 0 ] && [ $cmd5 -eq 0 ] && [ $cmd6 -eq 0 ] && [ $cmd7 -eq 0 ]; then
-    dialog --backtitle "Script Manutenzione Ubuntu" --title "Successo" --msgbox "Pulizia dei pacchetti eseguita con successo!" 0 0
+    dialog --msgbox "Pulizia dei pacchetti eseguita con successo!" 0 0
   else
-    dialog --backtitle "Script Manutenzione Ubuntu" --title "Errore" --msgbox "Ops...Qualcosa è andato storto!" 0 0
+    dialog --msgbox "Ops...Qualcosa è andato storto!" 0 0
   fi
-
-}
-
-ApriLog () {
-
-if [  -f ${logfile} ]; then
-	sudo -u ${utente} xdg-open ${logfile} > /dev/null 2>&1
-else
-	dialog --title "Errore" --backtitle "Script Manutenzione Ubuntu" --msgbox "Il file non esiste. Devi prima eseguire una delle operazioni di manutenzione" 0 0
-fi
-
-return
 
 }
 
@@ -250,8 +192,7 @@ Menu () {
     1 "Aggiornamento del sistema" off \
     2 "Eliminazione file temporanei" off \
     3 "Eliminazione pacchetti orfani e vecchi log" off \
-    4 "Riparazione pacchetti bloccati o danneggiati" off \
-    5 "Apri log delle operazioni eseguite" off )"
+    4 "Riparazione pacchetti bloccati o danneggiati" off)"
 
   resp=$?
   if [ $resp -eq 1 ]; then
@@ -264,8 +205,8 @@ Menu () {
           1) Aggiornamento ;;
           2) PuliziaTemporanei ;;
           3) PuliziaPacchetti ;;
-          4) RiparazionePacchetti ;;
-	  5) ApriLog;;
+          4) RiparazionePacchetti;;
+	  5) clear; exit ;;
         esac 
   done
 }
@@ -283,8 +224,6 @@ fi
 
 # Controlla se le dipendenze sono soddisfatte
 Requisiti
-
-InizializzaLog
 
 Disclaimer
 
